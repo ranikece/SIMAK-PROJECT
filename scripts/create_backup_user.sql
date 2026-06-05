54fd5009d5857363f3235cbc6374915286df7f56
ramapraditharyananda <ramapro2006@gmail.com>
2026-06-05 22:46:44 +0700

    Delete scripts/create_backup_user.sql

--- scripts/create_backup_user.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER IF NOT EXISTS 'adm_backup'@'localhost' IDENTIFIED BY 'admin123';
-GRANT SELECT, SHOW VIEW, TRIGGER, LOCK TABLES, EVENT ON simak_terpadu.* TO 'adm_backup'@'localhost';
-FLUSH PRIVILEGES;
