9a813e4b3fb35b24dd1cb995bf69e0f753ecc09d
Reggy Desvita <moochiiree@gmail.com>
2026-06-05 23:04:49 +0700

    Add files via upload

---
+++ scripts/pdt/deadlock_session_2.sql
@@ -0,0 +1,6 @@
+USE simak_terpadu;
+START TRANSACTION;
+UPDATE layanan SET kapasitas_harian = kapasitas_harian WHERE id_layanan = 2;
+DO SLEEP(10);
+UPDATE layanan SET kapasitas_harian = kapasitas_harian WHERE id_layanan = 1;
+COMMIT;
